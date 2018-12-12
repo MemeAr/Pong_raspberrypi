31bd71258c2318b81e45f3be2b3a9ed0046f1dfe
Unknown <amelie.ar98@gmail.com>
2018-12-12 13:45:40 +0100

    Afficher la raquette

--- pong_raspberrypi.py
+++ pong_raspberrypi.py
@@ -7,6 +7,7 @@ import random
 sense.clear(0,0,0)
 
 purple = (150, 117, 175)
+blue = (0, 249, 182)
 
 x = random.randint(1, 6)
 y = random.randint(1, 6)
@@ -14,11 +15,23 @@ y = random.randint(1, 6)
 x_sens = 1
 y_sens = 1
 
+rx = 0
+ry = 0
+
+ry_sens = 1
+
+def pongRaquette():
+  sense.set_pixel(rx, ry, blue)
+  sense.set_pixel(rx, ry_sens, blue)
+  sense.set_pixel(rx, ry_sens + 1, blue)
+
+
 sense.set_pixel(x, y, purple)
 
 while True:
   sense.clear(0,0,0)
   sense.set_pixel(x, y, purple)
+  pongRaquette()
   time.sleep(0.2)
   x = x + x_sens
   y = y + y_sens
@@ -35,3 +48,9 @@ while True:
 
   if y==7:
     y_sens = -1
+  
+
+    
+  
+
+  
