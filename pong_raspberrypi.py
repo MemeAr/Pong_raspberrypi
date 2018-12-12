bd880f263441e09f8c75c9757f9b74661f6ff3ca
Unknown <amelie.ar98@gmail.com>
2018-12-12 10:15:20 +0100

    Faire bouger la led

--- pong_raspberrypi.py
+++ pong_raspberrypi.py
@@ -1,7 +1,18 @@
-#Affiché une led#
 from sense_hat import SenseHat
 sense = SenseHat()
+import time #importer le time#
+
+sense.clear(0,0,0)
 
 purple = (150, 117, 175)
+x = 0
+y = 6
 
-sense.set_pixel(5, 5, purple)
+sense.set_pixel(x, y, purple) #Affiché une led#
+
+while x<7: #Faire bouger la led sur la ligne#
+  sense.clear(0,0,0)
+  sense.set_pixel(x, y, purple)
+  time.sleep(0.5) #temps#
+  x = x+1
+  
